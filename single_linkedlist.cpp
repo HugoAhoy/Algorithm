e93f3bc52b13dfec366153b8345f806ece592718
HugoAhoy <zhjd3302@outlook.com>
2018-09-27 21:16:17 +0800

    implement the functions

--- single_linkedlist.cpp
+++ single_linkedlist.cpp
@@ -7,6 +7,9 @@ template<typename T>
 class Node
 {
     friend class SingLinkList<T>;
+    public:
+    Node(const T& _elem);
+    Node();
     private:
     T elem;
     Node<T>* next;
@@ -18,13 +21,14 @@ class SingLinkList
 {
     private:
     Node<T> *head, *tail;
+    int elemNum;
 
     public:
     SingLinkList();
     virtual ~SingLinkList();
-    bool Insert(int position, const T& elem);
+    bool InsertAfter(int position, const T& elem);
     bool Delete(int position, T& elem);
-    bool Traverse();
+    bool Traverse(void (*func)(T& elem));
     void Clear();
     bool GetElem(int positon, T& elem) const;
     bool SetElem(int postion, const T& elem);
@@ -36,7 +40,193 @@ class SingLinkList
 
 //函数实现
 template<typename T>
+Node<T>::Node(const T& _elem)
+{
+    this->elem = _elem;
+    next = nullptr;
+}
+
+template<typename T>
+Node<T>::Node()
+{
+    elem = T();
+    next = nullptr;
+}
+
+template<typename T>
 SingLinkList<T>::SingLinkList()
 {
     head = tail = nullptr;
+    elemNum = 0;
+}
+
+template<typename T>
+bool SingLinkList<T>::InsertAfter(int position, const T& elem)
+{
+    if(position < 0 || position > elemNum)
+    {
+        return false;
+    }
+    else
+    {
+        Node<T>* p = new Node<T>(elem);
+        Node<T>* temp;
+        temp = head;
+        for(int i = 0; i < position - 1; i++)
+        {
+            temp = temp->next;
+        }
+        p->next = temp->next;
+        temp->next = p;
+        if(position == elemNum)
+        {
+            tail = p;
+        }
+        elemNum++;
+        return true;
+    }
+}
+
+template<typename T>
+bool SingLinkList<T>::Delete(int position, T& elem)
+{
+    if(position <= 0 || position > elemNum)
+    {
+        return false;
+    }
+    else
+    {
+        if(positon = 1)
+        {
+            Node<T> *save = head->next;
+            elem = head->elem;
+            delete head;
+            head = save;
+        }
+        else
+        {
+            Node<T> *save;
+            Node<T> * temp = head;
+            for(int i = 0; i < position - 2; i++)
+            {
+                temp = temp->next;
+            }
+            save = temp->next;
+            elem = save->elem;
+            temp->next = save->next;
+            elemNum--;
+            delete save;
+        }
+        return true;
+    }
+}
+
+template<typename T>
+bool SingLinkList<T>::Traverse(void (*func)(T& elem))
+{
+    Node<T> * temp = head;
+    for(int i = 0; i < elemNum; i++)
+    {
+        (*func)(temp->elem);
+        temp = temp->next;
+    }
+    return true;
+}
+
+template<typename T>
+void SingLinkList<T>::Clear()
+{
+    while(elemNum != 0)
+    {
+        T temp_use;
+        this->Delete(1, temp_use);
+    }
+    tail = nullptr;
+    return;
+}
+
+template<typename T>
+bool SingLinkList<T>::GetElem(int positon, T& elem) const
+{
+    if(position <= 0 || position > elemNum)
+    {
+        return false;
+    }
+    else
+    {
+        Node<T> * temp = head;
+        for(int i = 0; i < position - 1; i++)
+        {
+            temp = temp->next;
+        }
+        elem = temp->elem;
+        return true;
+    }
+}
+
+template<typename T>
+bool SingLinkList<T>::SetElem(int postion, const T& elem)
+{
+    if(position <= 0 || position > elemNum)
+    {
+        return false;
+    }
+    else
+    {
+        Node<T> * temp = head;
+        for(int i = 0; i < position - 1; i++)
+        {
+            temp = temp->next;
+        }
+        temp->elem = elem;
+        return true;
+    }
+}
+
+template<typename T>
+int SingLinkList<T>::Prior(T &elem, T &elem_get)
+{
+    Node<T> *temp = head->next;
+    T priorElem = head->elem;
+    for(int i = 1; i < elemNum; i++)
+    {
+        if(temp->elem == elem)
+        {
+            elem_get = priorElem;
+            return i;
+        }
+        else
+        {
+            priorElem = temp->elem;
+            temp = temp->next;
+        }
+    }
+    return -1;
+}
+
+template<typename T>
+int SingLinkList<T>::Next(T &elem, T &elem_get)
+{
+    Node<T> *temp = head;
+    T nextElem = head->next->elem;
+    for(int i = 1; i < elemNum; i++)
+    {
+        if(temp->elem == elem)
+        {
+            elem_get = nextElem;
+            return i + 1;
+        }
+        else
+        {
+            temp = temp->next;
+            nextElem = temp->next->elem;
+        }
+    }
+    return -1;
+}
+
+template<typename T>
+SingLinkList<T>::~SingLinkList()
+{
+    this->Clear();
 }
